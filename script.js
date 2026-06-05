1ff004e2c6476f877f68296f3390dd900aaa715e
yidengs392-art <yidengs392@gmail.com>
2026-06-05 17:24:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+cards.forEach(card => {
+    card.classList.add("hidden");
+    observer.observe(card);
+});
